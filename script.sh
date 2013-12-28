bdc71fbf61183d561c1b1517bd9b1ab091ccb5be
Marcel de Graaf <mail@marceldegraaf.net>
2013-12-28 17:17:18 +0100

    Do not use noninteractive mode

--- script.sh
+++ script.sh
@@ -12,13 +12,13 @@ sudo su deploy -c "curl -s ssh.keychain.io/mail@marceldegraaf.net/install | bash
 # Install Ruby
 sudo apt-add-repository ppa:brightbox/ruby-ng
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y ruby1.9.1
+sudo apt-get install -y ruby1.9.1
 
 # Install bundler
 sudo gem install bundler
 
 # Install support software
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential vim imagemagick wget git-core htop libmagickwand-dev libcurl4-openssl-dev libpcre3-dev ruby-dev libssl0.9.8 libmysql-ruby libmysqlclient-dev nodejs mysql-server
+sudo apt-get install -y build-essential vim imagemagick wget git-core htop libmagickwand-dev libcurl4-openssl-dev libpcre3-dev ruby-dev libssl0.9.8 libmysql-ruby libmysqlclient-dev nodejs mysql-server
 
 # Install Passenger
 sudo gem install passenger
